0f92d088074f58783f5f06aea2430efa3a1194c4
CyanSpaghetti <tp.arash.m@gmail.com>
2024-01-15 23:16:55 +0500

    added reading view

--- index.js
+++ index.js
@@ -1,8 +1,13 @@
 document.addEventListener('DOMContentLoaded', function () {
     const darkModeToggle = document.getElementById('darkModeToggle');
+    const readingModeToggle = document.getElementById('readingModeToggle');
     const body = document.body;
 
     darkModeToggle.addEventListener('click', function () {
         body.classList.toggle('dark-mode');
+    });
+
+    readingModeToggle.addEventListener('click', function () {
+        body.classList.toggle('reading-mode');
     });
 });
